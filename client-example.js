574751bd0820c4b152373f5f5daf6398b1223c68
Chris <28301107+christsim@users.noreply.github.com>
2019-11-29 23:20:49 +0200

    fixed websocket reconnects, also added a force reconnect time

--- client-example.js
+++ client-example.js
@@ -2,23 +2,23 @@ const { ValrV1RestClient, ValrV1WsClient } = require('./index.js')
 const util = require('util')
 
 // N.B. keep these safe.  don't commit to source control etc...
-var apiKey = '<your-api-key>',
-var apiSecret = '<your-api-secret>',
+const apiKey = process.env.VALR_API_KEY;         //'<your-api-key>',
+const apiSecret = process.env.VALR_API_SECRET;   //'<your-api-secret>',
 
 var valrClient = new ValrV1RestClient(apiKey, apiSecret)
 valrClient.account.getBalances().then((balance) => console.log(balance))
 
 // account
-var valrWsAccountClient = new ValrV1WsClient(apiKey, apiSecret, ValrV1WsClient.WSPATHS.ACCOUNT)
-valrWsAccountClient.connect();
-valrWsAccountClient.on('connected', () => console.log('ACCOUNT:', 'connected'));
-valrWsAccountClient.on('message', (data) => console.log('ACCOUNT:', data));
-valrWsAccountClient.on('error', (err) => console.log('ACCOUNT:', err));
-valrWsAccountClient.on('close', (code, reason) => console.log('ACCOUNT:', code, reason));
-valrWsAccountClient.on('disconnected', () => console.log('ACCOUNT:', 'disconnected'));
+// const valrWsAccountClient = new ValrV1WsClient(apiKey, apiSecret, ValrV1WsClient.WSPATHS.ACCOUNT)
+// valrWsAccountClient.connect();
+// valrWsAccountClient.on('connected', () => console.log('ACCOUNT:', 'connected'));
+// valrWsAccountClient.on('message', (data) => console.log('ACCOUNT:', data));
+// valrWsAccountClient.on('error', (err) => console.log('ACCOUNT:', err));
+// valrWsAccountClient.on('close', (code, reason) => console.log('ACCOUNT:', code, reason));
+// valrWsAccountClient.on('disconnected', () => console.log('ACCOUNT:', 'disconnected'));
 
 //trade
-var valrWsTradeClient = new ValrV1WsClient(apiKey, apiSecret, ValrV1WsClient.WSPATHS.TRADE)
+const valrWsTradeClient = new ValrV1WsClient(apiKey, apiSecret, ValrV1WsClient.WSPATHS.TRADE, { forceReconnectSeconds: 30 })
 valrWsTradeClient.connect();
 
 valrWsTradeClient.on('connected', () => console.log('TRADE:', 'connected'));
